0108212e2fc94e6abd94ab057b1a627b32d65e12
magique99 <dragos.tudose71@gmail.com>
2026-05-18 18:13:25 +0300

    add profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,18 +4,17 @@ import ProfilePostsV2 from "@/components/profile/ProfilePostsV2";
 export default function ProfilePage() {
   return (
      <main className="max-w-7xl mx-auto p-6">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+      
       <h1 className="text-3xl font-bold mb-8">
         Profilul meu
       </h1>
 
-      <div className="grid md:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
 
         <section className="border rounded-xl p-5">
           <ProfilePageV4 />
         </section>
 
-      </div>
       </div>
     </main>
   );
