4b52d67d1308154b1447865ed45abb8a69784eb6
magique99 <dragos.tudose71@gmail.com>
2026-05-24 19:10:28 +0300

    update UX

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import CharacterCount from "@tiptap/extension-character-count";
