09337f78efada20cd6f118e9b2c2528f4793a91a
magique99 <dragos.tudose71@gmail.com>
2026-05-24 19:07:10 +0300

    update UX

--- app/autori/page.tsx
+++ app/autori/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import type { Post, Profile } from "@/lib/types";
