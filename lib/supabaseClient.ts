78b4fbc225ea4631794449c69f7a3be3fd9e0ebd
magique99 <dragos.tudose71@gmail.com>
2026-05-26 20:25:06 +0300

    update UX

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "@/lib/database.types";
 
-export const supabase = createClient<Database>(
+export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 );
